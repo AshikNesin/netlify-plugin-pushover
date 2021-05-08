d288b193593dcde6237226479f63fddd2a479ead
Bharathvaj Ganesan <bharathvaj1995@gmail.com>
2021-05-08 18:33:18 +0530

    fix: fail plugin when api envs are not found

--- index.js
+++ index.js
@@ -10,33 +10,33 @@ const getSuccessMsg = () =>
 const getErrorMsg = () =>
     `Hi there, Latest build failed 😱\n\nCheck your build's log for more details\n\n👉 ${URL}`;
 
-const precheck = () => {
+const precheck = ({ utils }) => {
     if (!PUSHOVER_USER_KEY || !PUSHOVER_API_TOKEN) {
-        console.log(
+        utils.build.failPlugin(
             'PUSHOVER_USER_KEY or PUSHOVER_API_TOKEN is not available as environment variable'
         );
         return false;
     }
     return true;
 };
-const pluginFailureHandler = (error, utils) => utils.build.failPlugin(
-                    'Failed to send PushOver message',
-                    { error }
-                );
+
+const pluginFailureHandler = (error, { utils }) =>
+    utils.build.failPlugin('Failed to send Pushover message', { error });
+
 module.exports = {
-    async onSuccess({ utils }) {
-        if (precheck()) {
+    async onSuccess(pluginApi) {
+        if (precheck(pluginApi)) {
             console.log('Sending build success message via Pushover');
             const message = getSuccessMsg();
             try {
                 await sendPushOverNotification({ message });
             } catch (error) {
-                return pluginFailureHandler(error, utils);
+                return pluginFailureHandler(error, pluginApi);
             }
         }
     },
-    async onError({ utils }) {
-        if (precheck()) {
+    async onError(pluginApi) {
+        if (precheck(pluginApi)) {
             console.log('Sending build failed message via Pushover');
             const message = getErrorMsg();
             try {
@@ -46,7 +46,7 @@ module.exports = {
                     sound: 'siren',
                 });
             } catch (error) {
-                return pluginFailureHandler(error, utils);
+                return pluginFailureHandler(error, pluginApi);
             }
         }
     },
